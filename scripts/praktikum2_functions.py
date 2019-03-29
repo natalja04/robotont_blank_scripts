be4b754ddc4292fe0a227d901faa2a1865ba2ec4
„natalja04 <natalja04@mail.ru>
2019-03-29 16:41:39 +0200

    “paev1”

--- scripts/praktikum2_functions.py
+++ scripts/praktikum2_functions.py
@@ -22,7 +22,13 @@ def closing():
 #######################
 # YOUR FUNCTIONS HERE #
 #######################
-
+def otse():
+    for i in range(0,30):
+      vel_msg.linear.x = 0.2
+      vel_msg.linear.y = 0
+      vel_msg.angular.z = 0
+      velocity_publisher.publish(vel_msg)
+      rospy.sleep(0.1)
 
 ###########################
 # YOUR FUNCTIONS HERE END #
